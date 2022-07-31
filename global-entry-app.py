3b52590b7880c4307afa8d7027ea933fdea3539f
Robert Burton <robert@robert.red>
2022-07-31 00:18:42 -0400

    Added locations and message box

--- global-entry-app.py
+++ global-entry-app.py
@@ -3,18 +3,29 @@
 
 import pip._vendor.requests as requests
 import time
+import sys
+import ctypes
 
 APPOINTMENTS_URL = "https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=1&locationId={}&minimum=1"
 LOCATION_IDS = {
-    'Richmond': 7540,
-    'Charlotte': 14321,
+    'Bowling Green NYC': 6480,
+    'Jamaica JFK': 5140,
+    'Newark NJ': 5444,
+    #'Anchorage AK': 7540, 
 }
+SLEEP_TIME = 130
 
-for city, id in LOCATION_IDS.items():
-    url = APPOINTMENTS_URL.format(id)
-    appointments = requests.get(url).json()
-    if appointments:
-        print("{}: Found an appointment at {}!".format(city, appointments[0]['startTimestamp']))
-    else:
-        print("{}: No appointments available".format(city))
-    time.sleep(1)
+try:
+    while True:
+        for city, id in LOCATION_IDS.items():
+            url = APPOINTMENTS_URL.format(id)
+            appointments = requests.get(url).json()
+            if appointments:
+                print("{}: Found an appointment at {}!".format(city, appointments[0]['startTimestamp']))
+                ctypes.windll.user32.MessageBoxW(0, "{}: Found an appointment at {}. Go to https://ttp.cbp.dhs.gov/ now!".format(city, appointments[0]['startTimestamp']), "Global Entry Appointment", 1)
+            else:
+                print("{}: No appointments available".format(city))
+            time.sleep(1)
+        time.sleep(SLEEP_TIME)
+except KeyboardInterrupt:
+    sys.exit(0)
